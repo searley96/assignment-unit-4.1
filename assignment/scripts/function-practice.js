1362926d99a10c9924712753edd3f0cf92204d42
Siv Earley <searley96@gmail.com>
2023-02-12 18:43:28 -0500

    formatting changes

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -14,53 +14,53 @@ console.log('Test - should say "Hello World!"', hello());
 
 // 2. Function to return an personalized hello, using the `name` argument.
 //    for example 'Hello, Jo!', or 'Hello, Stacy!'
-function helloName( name ) {
+function helloName(name) {
   return 'Hello Siv!';
 }
 // Remember to call the function to test
 console.log('Test - should say "Hello Siv!"', helloName());
 
 // 3. Function to add two numbers together & return the result
-function addNumbers( firstNumber, secondNumber ) {
+function addNumbers(firstNumber, secondNumber) {
   // return firstNumber + secondNumber;
   return firstNumber + secondNumber;
 }
-console.log('Should return 10:', addNumbers(5,5));
+console.log('Should return 10:', addNumbers(5, 5));
 
 // 4. Function to multiply three numbers & return the result
-function multiplyThree( num1, num2, num3) {
+function multiplyThree(num1, num2, num3) {
   return num1 * num2 * num3;
 }
 console.log('Should return 125:', multiplyThree(5, 5, 5));
 
 // 5. Function that will return true if a number is positive, 
 //    or greater than zero, and false otherwise
-function isPositive( number ) {
-  if ( number > 0 ){
+function isPositive(number) {
+  if (number > 0) {
     return true;
   } else {
     return false;
- } 
+  }
 }
 console.log('Should return true:', isPositive(2))
 
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( 'isPositive - should say true', isPositive(3) );
-console.log( 'isPositive - should say false', isPositive(0) );
-console.log( 'isPositive - should say false', isPositive(-3) );
+console.log('isPositive - should say true', isPositive(3));
+console.log('isPositive - should say false', isPositive(0));
+console.log('isPositive - should say false', isPositive(-3));
 
 
 // 6. Function to return the _last_ item in an array. If the 
 //    array is empty, return `undefined`.
 
 
-function getLast( array ) {
- if (array.length > 0) {
- return array[array.length - 1];
- } else {
-  return 'Undefined.'
- }
+function getLast(array) {
+  if (array.length > 0) {
+    return array[array.length - 1];
+  } else {
+    return 'Undefined.'
+  }
 }
 
 console.log("should print 3:", getLast([1, 5, 2, 3]));
@@ -68,13 +68,13 @@ console.log("should print 3:", getLast([1, 5, 2, 3]));
 // 7. Function to find a value in an array. Return true if the 
 //    value is found and false otherwise. Use a loop;
 //    DO NOT use Array.includes, Array.indexOf, or Array.find 
-function find( value, array ) {
+function find(value, array) {
   for (let i = 0; i < array.length; i++) {
-  if (array[i] === value) {
-  return true;
+    if (array[i] === value) {
+      return true;
+    }
   }
- }
- return false;
+  return false;
 }
 
 console.log("should print true:", find(5, [1, 2, 5, 2, 3]));
@@ -84,13 +84,17 @@ console.log("should print true:", find(5, [1, 2, 5, 2, 3]));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (string.charAt(0) === letter) {
+    return true;
+  } else {
+    return false;
+  }
 }
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
+console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
 
 // 9. Function to return the sum of all numbers in an array
-function sumAll( ) {
+function sumAll() {
   let sum = 0
   // TODO: loop to add items
   return sum;
@@ -99,6 +103,10 @@ function sumAll( ) {
 // 10. Function to return a new array of all positive (greater than zero)
 //     numbers contained in an input array. If there are no positive numbers
 //     return an empty array. Note: The input array should not change.
+
+newArray = [2, -1, 0, 3, -3];
+
+function returnPositive ()
 
 
 
